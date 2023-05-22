cda08d5f745ecca47ce2913c70ecdb89feec868f
VarelaTechDev <varela.tech.dev@gmail.com>
2023-05-21 17:13:58 -0700

    Task complete

--- BookStoreSchema.sql
+++ BookStoreSchema.sql
@@ -70,7 +70,8 @@ INSERT INTO books (bookName, bookPubYear, bookAuthorId) VALUES
 ('Book 12', 2032, 2),
 ('Book 13', 2033, 3),
 ('Book 14', 2034, 4),
-('Book 15', 2035, 5);
+('Book 15', 2035, 5),
+('Book 16', 2035, 3);
 
 UPDATE books
 SET bookName = 'New Book Name', bookPubYear = 2035
@@ -78,9 +79,69 @@ WHERE bookId = 1;
 
 
 
+-- SELECT * FROM books
+-- INNER JOIN author 
+-- ON books.bookAuthorId = author.authorId;
+
+
+
+
+-- Write SQL queries to perform the following operations:
+
+
+
+-- 1. Insert new authors and books into the database.
+INSERT INTO books(bookName, bookPubYear) VALUES
+('Book One', 1990),
+('Book Two', 1238),
+('Book Three', 4233),
+('Book Four', 3434);
+
+
+
 SELECT * FROM books
-INNER JOIN author 
-HAVING books.bookAuthorId = author.authorId;
+LEFT JOIN author 
+ON books.bookAuthorId = author.authorId;
+
+-- We would need to join to allow us to use an author name
+-- 2. Find all books written by a particular author.
+SELECT * FROM books
+INNER JOIN author
+ON books.bookAuthorId = author.authorId
+WHERE author.authorName = 'Sophia Parker';
+
+
+-- SELECT COUNT(bookName) FROM books
+-- INNER JOIN author
+-- ON books.bookAuthorId = author.authorId
+-- WHERE author.authorName = 'Sophia Parker';
+
+-- This gives us 6
+SELECT COUNT(bookName) FROM books
+INNER JOIN author
+ON books.bookAuthorId = author.authorId;
+
+-- 3. Find the author who wrote the most books.
+SELECT authorName, COUNT(bookName) AS bookCount FROM author
+LEFT JOIN books ON author.authorId = books.bookAuthorId
+GROUP BY authorId
+ORDER BY bookCount DESC
+LIMIT 1;
+
+
+-- 4. Update the email of a customer.
+SELECT * FROM customer;
+
+-- Error Code: 1175. You are using safe update mode and you tried to update a table without a WHERE that uses a KEY column.  To disable safe mode, toggle the option in Preferences -> SQL Editor and reconnect.
+SET SQL_SAFE_UPDATES=0;
+
+
+UPDATE customer
+SET customerEmail = 'e@example.com'
+WHERE customerEmail = 'olivia@example.com';
+
+
+SELECT * FROM customer;
 
 
 
